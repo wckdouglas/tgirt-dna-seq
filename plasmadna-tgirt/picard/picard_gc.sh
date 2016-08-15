16ba5351801e38d767084ac306233d9ecd86f833
wckdouglas <wckdouglas@gmail.com>
2016-08-14 19:30:24 -0500

    Sun Aug 14 19:30:24 CDT 2016

--- plasmadna-tgirt/picard/picard_gc.sh
+++ plasmadna-tgirt/picard/picard_gc.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
 
-PROJECT_PATH=${WORK}/cdw2854/plasmaDNA
+# get variable
+BAM=$1
+PROJECT_PATH=$(dirname $BAM)
+REF=$2
+
+#make directory
+SAMPLENAME=$(basename ${BAM%.bam})
 BAM_PATH=${PROJECT_PATH}/bamFiles
 SORTED_BAM_PATH=${BAM_PATH}/sorted
 RESULT_PATH=${PROJECT_PATH}/picard_results
 FIGURES_PATH=${PROJECT_PATH}/figures
-REF_PATH=${REF}/GRCh38/hg38_rDNA
-REF=${REF_PATH}/genome_rDNA.fa
 mkdir -p ${RESULT_PATH} ${FIGURES_PATH} ${SORTED_BAM_PATH}
 
-for BAM in ${BAM_PATH}/*bam
-do
-    SAMPLENAME=$(basename ${BAM%.bam})
-	if [[ $SAMPLENAME != G* ]]
-	then
-		echo Running $SAMPLENAME
-		picard SortSam \
-			INPUT=${BAM} \
-			OUTPUT=/dev/stdout 
-			SORT_ORDER=coordinate \
-			> ${SORTED_BAM_PATH}/${SAMPLENAME}.sorted.bam 
-		echo Sorted $SAMPLENAME
-		picard MarkDuplicates \
-			INPUT=/dev/stdin \
-			OUTPUT=/dev/stdout \
-			METRICS_FILE=${RESULT_PATH}/${SAMPLENAME}.duplicate.txt \
-			REMOVE_DUPLICATES=True \
-			ASSUME_SORTED=true \
-			> ${SORTED_BAM_PATH}/${SAMPLENAME}.sorted.rmdup.bam 
-		echo Removed duplicates $SAMPLENAME
-		echo Collecting Bias
-		picard CollectGcBiasMetrics \
-			VALIDATION_STRINGENCY=LENIENT \
-			SCAN_WINDOW_SIZE=100 \
-			INPUT=/dev/stdin \
-			CHART_OUTPUT=${FIGURES_PATH}/${SAMPLENAME}.pdf  \
-			OUTPUT=${RESULT_PATH}/${SAMPLENAME}.txt \
-			SUMMARY_OUTPUT=${RESULT_PATH}/${SAMPLENAME}.GC.summary  \
-			REFERENCE_SEQUENCE=$REF \
-			ASSUME_SORTED=true 
-	fi
-done
+
+echo Using project directory: $PROJECT_PATH
+echo Running $SAMPLENAME
+
+SORTED_BAM=${SORTED_BAM_PATH}/${SAMPLENAME}.sorted.bam 
+picard SortSam \
+	INPUT=${BAM} \
+	OUTPUT=$SORTED_BAM  \
+	SORT_ORDER=coordinate 
+echo Sorted $SAMPLENAME
+
+RMDUP_BAM=${SORTED_BAM_PATH}/${SAMPLENAME}.sorted.rmdup.bam
+picard MarkDuplicates \
+	INPUT=$SORTED_BAM \
+	OUTPUT=$RMDUP_BAM \
+	METRICS_FILE=${RESULT_PATH}/${SAMPLENAME}.duplicate.txt \
+	REMOVE_DUPLICATES=True \
+	ASSUME_SORTED=true 
+echo Removed duplicates $SAMPLENAME
+
+echo Collecting Bias
+picard CollectGcBiasMetrics \
+	VALIDATION_STRINGENCY=LENIENT \
+	SCAN_WINDOW_SIZE=100 \
+	INPUT=$RMDUP_BAM \
+	CHART_OUTPUT=${FIGURES_PATH}/${SAMPLENAME}.pdf  \
+	OUTPUT=${RESULT_PATH}/${SAMPLENAME}.txt \
+	SUMMARY_OUTPUT=${RESULT_PATH}/${SAMPLENAME}.GC.summary  \
+	REFERENCE_SEQUENCE=$REF \
+	ASSUME_SORTED=true 
+echo Finished $SAMPLENAME
