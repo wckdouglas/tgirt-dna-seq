a42fdd9ac2233038c7b1f3da8905ee47605e8ab5
wckdouglas <wckdouglas@gmail.com>
2017-04-21 03:18:58 -0500

    clean up

--- plasmadna-tgirt/window_protection_scores/fpkm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python extractFpkmTss.py \
-    /scratch/02727/cdw2854/plasma_project/TGIRT_plasma_dna/assemblies/RNA-OCD.gtf \
-    | uniq \
-    | sort -k8nr \
-    | awk '$8>0 && $2>0' \
-    > /scratch/02727/cdw2854/plasma_project/TGIRT_plasma_dna/assemblies/genes.tss.bed
